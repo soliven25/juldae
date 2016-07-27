8d1163bc6a509d3f5185a7347b815971a1978274
Sungkeun Choi <soliven25@gmail.com>
2016-07-27 11:24:51 +0900

    add FollowCam Script to move camera

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -5,14 +5,29 @@ public class PlayerCtrl : MonoBehaviour {
 
 	private float h = 0.0f;
 	private float v = 0.0f;
+	private Transform tr;
+	public float moveSpeed = 10.0f;
+	public float rotSpeed = 100.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		tr = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+
+		Debug.Log("H = " + h.ToString());
+		Debug.Log("V = " + v.ToString());
+
+		Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h); 
+
+		//tr.Translate(Vector3.forward * moveSpeed * v * Time.deltaTime, Space.Self);
+		tr.Translate(moveDir * Time.deltaTime * moveSpeed, Space.Self);
+
+		tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
 	
 	}
 }
