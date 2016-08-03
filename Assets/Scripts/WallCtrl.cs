afe5d8b427df9bfc864019700fb258752ad22e2b
Sungkeun Choi <soliven25@gmail.com>
2016-08-03 12:16:44 +0900

    add Bullet

---
+++ Assets/Scripts/WallCtrl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision coll)
+	{
+		if(coll.collider.tag == "BULLET")
+		{
+			Destroy(coll.gameObject);
+		}
+	}
+}
